2aa76154c497bfee49ac34fb06d81709fc0b4175
Hanspeter Gisler <hanspeter.gisler@gisler.pro>
2024-11-30 09:20:14 +0100

    simplify code and strip off non-required part

--- app/src/delay.c
+++ app/src/delay.c
@@ -23,7 +23,7 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-//#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 #define CONFIG_ZMK_DELAY_MAX_KEY_POSITIONS_DELAYABLE 12
 
@@ -44,7 +44,7 @@ struct {
     // array: sort order ascending according key_positions_pressed timestamp (occurrence)
     struct zmk_position_state_changed_event ev[CONFIG_ZMK_DELAY_MAX_KEY_POSITIONS_DELAYABLE];
     struct zmk_position_state_changed_event *oldest, *next;
-} past_presses;
+} captured_presses;
 
 struct k_work_delayable timeout_task;
 
@@ -55,65 +55,77 @@ static void recede(struct zmk_position_state_changed_event** it);
 static int initialize_delay(struct delay_cfg *delay_cfg) {
   // I assume here, that only one delay config exists on keymap file
   delay_config = delay_cfg;
-    return 0;
+  captured_presses.next = &captured_presses.ev[0]; 
+  captured_presses.oldest = NULL; 
+  return 0;
 }
 
 static int cleanup();
+static void update_timeout_task();
 
-static int capture_pressed_key(const struct zmk_position_state_changed *evdata) {
-  if (past_presses.next == past_presses.oldest) {
+static int capture_key_press(const struct zmk_position_state_changed *evdata) {
+  if (captured_presses.next == captured_presses.oldest) {
     // no space left in ring buffer
     LOG_ERR("Unable to delay position down event; already %d delayed. Increase "
             "CONFIG_ZMK_DELAY_MAX_KEY_POSITIONS_DELAYABLE",
             CONFIG_ZMK_DELAY_MAX_KEY_POSITIONS_DELAYABLE);
     return ZMK_EV_EVENT_BUBBLE;
   }
-  *past_presses.next = copy_raised_zmk_position_state_changed(evdata);
-  if (past_presses.oldest == NULL) {
-    past_presses.oldest = past_presses.next;
+  *captured_presses.next = copy_raised_zmk_position_state_changed(evdata);
+  if (captured_presses.oldest == NULL) {
+    captured_presses.oldest = captured_presses.next;
+    update_timeout_task();
   }
-  advance(&past_presses.next);
+  advance(&captured_presses.next);
   return ZMK_EV_EVENT_CAPTURED;
 }
 
-static bool release_oldest_of_past_presses() {
-  if (!past_presses.oldest) {
+static bool free_oldest_captured_key_press() {
+  if (!captured_presses.oldest) {
     return false;
   }
-  ZMK_EVENT_RAISE_AFTER(*past_presses.oldest, delay);
-  advance(&past_presses.oldest);
-  if (past_presses.oldest == past_presses.next)
-    past_presses.oldest = NULL;
+  ZMK_EVENT_RAISE_AFTER(*captured_presses.oldest, delay);
+  advance(&captured_presses.oldest);
+  if (captured_presses.oldest == captured_presses.next) {
+    captured_presses.oldest = NULL;
+  }
+  update_timeout_task();
   return true;
 }
 
-static void release_captured_key_presses(struct zmk_position_state_changed *evdata) {
-  if (past_presses.oldest == NULL) {
-    // no space left in ring buffer
+static void free_captured_key_presses(struct zmk_position_state_changed *evdata) {
+  if (captured_presses.oldest == NULL) {
     LOG_ERR("No captured key position event to release; ring buffer is empty");
     return;
   }
-  struct zmk_position_state_changed_event *it_released = past_presses.next;
+  struct zmk_position_state_changed_event *it_released = captured_presses.next;
   do {
     recede(&it_released);
-  } while (it_released->data.position != evdata->position);
+  } while (it_released->data.position != evdata->position && it_released != captured_presses.next);
+  
+  if (it_released == captured_presses.next) {
+    LOG_ERR("Came full circle, did not find pos in captured positions list.");
+    return;
+  }
+  advance(&it_released); // because in next loop, after ||: oldest is advanced..
+  // ..as well and we want to release including it_released.
   while (true) {
-    if (!release_oldest_of_past_presses() || past_presses.oldest == it_released)
+    if (!free_oldest_captured_key_press() || captured_presses.oldest == it_released)
       break;
   };
 }
 
 static void advance(struct zmk_position_state_changed_event** it) {
   (*it)++;
-  if (*it == &past_presses.ev[CONFIG_ZMK_DELAY_MAX_KEY_POSITIONS_DELAYABLE]) {
-    *it = &past_presses.ev[0];
+  if (*it == &captured_presses.ev[CONFIG_ZMK_DELAY_MAX_KEY_POSITIONS_DELAYABLE]) {
+    *it = &captured_presses.ev[0];
   }
 }
 
 static void recede(struct zmk_position_state_changed_event** it) {
   (*it)--;
-  if (*it < &past_presses.ev[0]) {
-    *it = &past_presses.ev[CONFIG_ZMK_DELAY_MAX_KEY_POSITIONS_DELAYABLE - 1];
+  if (*it < &captured_presses.ev[0]) {
+    *it = &captured_presses.ev[CONFIG_ZMK_DELAY_MAX_KEY_POSITIONS_DELAYABLE - 1];
   }
 }
 
@@ -123,11 +135,11 @@ static int cleanup() {
 }
 
 static void update_timeout_task() {
-  if (past_presses.oldest == NULL) {
+  if (captured_presses.oldest == NULL) {
     cleanup();
     return;
   }
-  int64_t time_since_oldest_ms = k_uptime_get() - past_presses.oldest->data.timestamp;
+  int64_t time_since_oldest_ms = k_uptime_get() - captured_presses.oldest->data.timestamp;
   int64_t due_in_ms = delay_config->timeout_ms - time_since_oldest_ms;
   if (due_in_ms > 1) {
     k_work_schedule(&timeout_task, K_MSEC(due_in_ms));
@@ -136,37 +148,13 @@ static void update_timeout_task() {
   k_work_schedule(&timeout_task, K_MSEC(1)); // fallback
 }
 
-bool is_delayed_keypos(struct zmk_position_state_changed *evdata) {
+bool is_keypos_with_delay(struct zmk_position_state_changed *evdata) {
   for (int p = 0; p < delay_config->layer_key_positions_len ; p++) {
     if (evdata->position == delay_config->layer_key_positions[p]) {
-      return false;
+      return false; // negativ logic
     }
   }
   return true;
-}
-
-static int position_state_down(struct zmk_position_state_changed *evdata) {
-  if (!is_delayed_keypos(evdata)) {
-    return ZMK_EV_EVENT_BUBBLE;
-  }
-  int evt = capture_pressed_key(evdata);
-  update_timeout_task();
-  return evt;
-}
-
-static int position_state_up(struct zmk_position_state_changed *evdata) {
-  if (is_delayed_keypos(evdata)) {
-    release_captured_key_presses(evdata);
-  }
-  return ZMK_EV_EVENT_BUBBLE;
-}
-
-static void delay_timeout_handler(struct k_work *item) {
-  if (!release_oldest_of_past_presses()) {
-    cleanup();
-    return;
-  }
-  update_timeout_task();
 }
 
 static int position_state_changed_listener(const zmk_event_t *ev) {
@@ -175,10 +163,20 @@ static int position_state_changed_listener(const zmk_event_t *ev) {
         return 0;
     }
     if (evdata->state) { // key down
-        return position_state_down(evdata);
+      if (!is_keypos_with_delay(evdata)) {
+        return ZMK_EV_EVENT_BUBBLE;
+      }
+      return capture_key_press(evdata);
     } else { // key up
-        return position_state_up(evdata);
+      if (is_keypos_with_delay(evdata)) {
+        free_captured_key_presses(evdata);
+      }
+      return ZMK_EV_EVENT_BUBBLE;
     }
+}
+
+static void delay_timeout_handler(struct k_work *item) {
+  free_oldest_captured_key_press();
 }
 
 ZMK_LISTENER(delay, position_state_changed_listener);
@@ -197,12 +195,10 @@ DT_INST_FOREACH_CHILD(0, DELAY_INST)
 
 static int delay_init() {
     k_work_init_delayable(&timeout_task, delay_timeout_handler);
-    past_presses.next = &past_presses.ev[0]; 
-    past_presses.oldest = NULL; 
     DT_INST_FOREACH_CHILD(0, INITIALIZE_DELAY);
     return 0;
 }
 
 SYS_INIT(delay_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
 
-//#endif
+#endif
